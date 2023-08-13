6d6007aa594b36d9332f1b9effdf660584efefb3
Rohan Srivastava <rohansrivastava.lko.up@gmail.com>
2023-08-13 21:53:45 +0530

    register component started

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,26 +1,156 @@
-import React from "react"
-import styled from 'styled-components'
-function Register() {
-  const HandleSubmit=((e)=>{
-    e.preventDefault();
-    alert("form");
-  })
-  const HandleChange=((e)=>{
+import React, { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+import logo from "../Assets/logo.svg"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 
+function Register() {
+  const [values, setValues ] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   })
-  return <>
-    <FormContainer>
-      <form onSubmit={(e)=>{
-         HandleSubmit(e)
-      }}>
-        <div className="brand">
-        <img  src="" alt="img"><h1>snappy</h1></img>
-        </div>
-        <input type="text" placeholder="Username" name="Username" onChange={e=>{HandleChange(e)}}></input>
-      </form>
-    </FormContainer>
-  </>
+  const HandleSubmit = (e) => {
+    e.preventDefault()
+    HandleValidation()
+  }
+  const HandleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
+  }
+  const HandleValidation = () => {
+    const { password, confirmPassword, username, email } = values
+    if (password !== confirmPassword) {
+      toast.error("password and confirm password doesnot match", {
+        autoClose: 2000,
+        draggabl: true,
+        position: "top-right",
+        theme: "dark",
+      })
+    }
+  }
+  return (
+    <>
+      <FormContainer>
+        <form
+          onSubmit={(e) => {
+            HandleSubmit(e)
+          }}
+        >
+          <div className="brand">
+            <img src={logo} alt="logo" />
+            <h1>Flik</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            onChange={(e) => {
+              HandleChange(e)
+            }}
+          ></input>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => {
+              HandleChange(e)
+            }}
+          ></input>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => {
+              HandleChange(e)
+            }}
+          ></input>
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => {
+              HandleChange(e)
+            }}
+          ></input>
+          <button type="submit"> Create User</button>
+          <span>
+            Already have an Account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+      <ToastContainer/>
+    </>
+  )
 }
 
-const FormContainer=styled.div``;
+const FormContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .brand {
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    justify-content: center;
+    img {
+      height: 5rem;
+    }
+    h1 {
+      color: white;
+      text-transform: uppercase;
+    }
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    flex-direction: column;
+    background-color: #00000076;
+    border-radius: 2rem;
+    padding: 3rem 5rem;
+    input {
+      background-color: transparent;
+      padding: 1rem;
+      border: 0.1rem solid #4e0eff;
+      border-radius: 0.4rem;
+      color: white;
+      width: 100%;
+      font-size: 1rem;
+      &:focus {
+        border: 0.1rem solid #997af0;
+        outline: none;
+      }
+    }
+    button {
+      background-color: #997af0;
+      color: white;
+      padding: 1rem 2rem;
+      border: none;
+      font-weight: bold;
+      cursor: pointer;
+      font-size: 1rem;
+      border-radius: 0.4rem;
+      text-transform: uppercase;
+      &:hover {
+        background-color: #4e0eff;
+        transition: 0.5s ease-in-out;
+      }
+    }
+    span {
+      color: white;
+      text-transform: uppercase;
+      a {
+        color: #4e0eff;
+        text-decoration: none;
+        font-weight: bold;
+      }
+    }
+  }
+`
 export default Register
